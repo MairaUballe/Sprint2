16f0ca849ed16bab4b41402562d5d27873e4b626
MairaUballe <46536481+MairaUballe@users.noreply.github.com>
2020-12-21 19:15:48 -0500

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ origins = [
     "http://localhost", "http://localhost:8080", "http://localhost:8081, https://test-sprint2.herokuapp.com/",
 ]
 api.add_middleware(
-    CORSMiddleware, allow_origins=origins,
+    CORSMiddleware, allow_origins=["*"],
     allow_credentials=True, allow_methods=["*"], allow_headers=["*"],
 )
 
